2cf1ff8b9cef21e9ed62e071ea7d01088be54e50
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-21 20:27:07 +0300

    Add Connection as a member and populate if in ctor. Make all prints via View and not sout

--- src/main/java/storesgroup/model/ChainAndMall.java
+++ src/main/java/storesgroup/model/ChainAndMall.java
@@ -8,30 +8,31 @@ import java.sql.*;
 public class ChainAndMall {
 
 
-    Controller controller = new Controller();
+    Controller controller;
+    View view;
+    Connection conn;
+
+    public ChainAndMall(View view, Controller controller) throws SQLException {
+        this.view = view;
+        this.controller = controller;
+        conn = controller.getConnectionToDB();
+    }
 
     /**
      * create new chain of stores to the database.
      * @param chainName - String icludes the chain nmae
      */
-    public void createChain(String chainName) {
+    public void createChain(String chainName) throws SQLException {
         View view = new View();
-        try (Connection conn = controller.getConnectionToDB();
-             PreparedStatement stmt = conn.prepareStatement("insert into chain (Name) values (?)")
-        ) {
+
+            PreparedStatement stmt = conn.prepareStatement("insert into chain (Name) values (?)");
             stmt.setString(1, chainName);
             int result = stmt.executeUpdate();
             if (result == 0) {
-                view.printMessageToConsole("no updates were done");
+                view.printMessage("no updates were done");
                 } else {
-                view.printMessageToConsole("New chain was inserted to DB  " + chainName);
+                view.printMessage("New chain was inserted to DB  " + chainName);
             }
-
-
-        } catch (SQLException e) {
-            System.out.println(e.getErrorCode());
-        }
-
     }
 
     public void viewAllChains() {
@@ -42,15 +43,24 @@ public class ChainAndMall {
             System.out.println("Displayed chains  :  ");
 
             while (rs.next()) {
-                System.out.print("Chain ID : " + rs.getString(1));
-                System.out.println("   Chain Name : " + rs.getString(2));
-
+                view.printMessage("" + rs.getString(1)+ "\t" + rs.getString(2));
             }
 
 
         } catch (SQLException e) {
             System.out.println(e.getErrorCode());
         }
+    }
+
+    public void viewAllMalls() throws SQLException {
+
+         Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery("Select id as ID,name as Name from shoppingmalls;");
+         view.printMessage("Displayed malls  :  ");
+
+            while (rs.next()) {
+                view.printMessage("" + rs.getString(1)+ "\t" + rs.getString(2));
+            }
     }
 }
 
