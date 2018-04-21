8278796188c4dfaef41b3656cc17721e459c08a1
Dorit Rieur <dr9081@att.com>
2018-04-21 10:23:41 +0300

    adjust to new SQL structure
    split add employee to add to chain/store

--- src/main/java/storesgroup/model/ChainAndMall.java
+++ src/main/java/storesgroup/model/ChainAndMall.java
@@ -15,29 +15,36 @@ public class ChainAndMall {
      * create new chain of stores to the database.
      * @param chainName - String icludes the chain nmae
      */
-    public void createChain(String chainName) {
+    public boolean createChain(String chainName) {
         try (Connection conn = controller.getConnectionToDB();
-             PreparedStatement stmt = conn.prepareStatement("insert into `excercise_biq`.`chain` (Name) values (?);");
+             PreparedStatement stmt = conn.prepareStatement("insert into `stores`.`chain` (name) values (?);");
         ) {
-            stmt.setString(1, chainName);
-            int result = stmt.executeUpdate();
-            if (result == 0) {
-                printMessageToConsole("no updates were done");
+
+            if (controller.selectFromDatabase("chain","name=\""+chainName+"\"","name")==null){
+                stmt.setString(1, chainName);
+                int result = stmt.executeUpdate();
+                if (result == 0) {
+                    printMessageToConsole("no updates were done");
                 } else {
-                printMessageToConsole("New chain was inserted to DB  " + chainName);
+                    printMessageToConsole("New chain was inserted to DB  " + chainName);
+                }
+                return true;
             }
+
+
 
 
         } catch (SQLException e) {
             System.out.println(e.getErrorCode());
+            return false;
         }
-
+return false;
     }
 
     public void viewAllChains() {
 
         try (Connection conn = controller.getConnectionToDB(); Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("Select idChain as ID,Name as Name from chain;")
+             ResultSet rs = stmt.executeQuery("Select idchain as ID,Name as Name from chain;")
         ) {
             System.out.println("Displayed chains  :  ");
 
--- src/main/java/storesgroup/model/Employee.java
+++ src/main/java/storesgroup/model/Employee.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class Employee {
 
@@ -15,45 +16,26 @@ public class Employee {
     Store store = new Store();
 
     /**
-     * add employee to chain or to store
+     * add employee to chain
      *
-     * @param employeeName
-     * @param toChain      - true - to chain, false = to store
+     * @param firstName - employee name
+     * @param chainID   - selected chain ID
      */
-    public void addEmployee(String employeeName, boolean toChain) {
+    public void addEmployeeToChain(String firstName,
+                                   int chainID, String lastName, String fname) {
 
-        int selectedValue = 0;
         try (Connection conn = controller.getConnectionToDB();
-             PreparedStatement stmt = conn.prepareStatement("insert into `excercise_biq`.`employee` (ID,Name,isManagement,storeID,chainID) values (?,?,?,?,?);");
+             PreparedStatement stmt = conn.prepareStatement("insert into `stores`.`employees` (id,first_name,isManager,chainID,last_name,fname) values (?,?,?,?,?,?);")
         ) {
-            stmt.setString(2, employeeName);
-            stmt.setString(1, getGenerateEmployeeID(5));
+            stmt.setInt(1, getGenerateEmployeeID(5));
+            stmt.setString(2, firstName);
+            stmt.setBoolean(3, true);
+            stmt.setString(5, lastName);
+            stmt.setString(6, fname);
 
 
-            if (toChain) {
-                stmt.setBoolean(3, true);
-                stmt.setInt(4, 99);
 
-                chainAndMall.viewAllChains();
-                System.out.println("Select a chain from the Available chains:  ");
-                selectedValue = controller.selectFromScanner();
-                stmt.setInt(5, selectedValue);
-
-            } else {
-                stmt.setBoolean(3, false);
-
-
-                store.viewAllStores();
-                System.out.println("Select a Store from the Available stores:  ");
-                selectedValue = controller.selectFromScanner();
-                stmt.setInt(4, selectedValue);
-
-// set chain according to the store selected
-
-                stmt.setInt(5, Integer.valueOf(store.getChainIDfromStore(selectedValue)));
-            }
-
-
+            stmt.setInt(4, chainID);
             int result = stmt.executeUpdate();
             if (result == 0) {
                 System.out.println("no updates were done");
@@ -61,35 +43,111 @@ public class Employee {
                 System.out.println("number of inserted records:  " + result);
             }
 
-
         } catch (SQLException e) {
             System.out.println(e.getErrorCode());
         }
 
     }
 
+    /**
+     * add employee to store
+     *
+     * @param employeeName - name
+     * @param storeID      - id of the selected store
+     */
+    public void addEmployeeToStore(String employeeName, int storeID, String lastName, String fname) {
 
-    private String getGenerateEmployeeID(int count) {
 
-
-        final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder builder = new StringBuilder();
-
-        while (count-- != 0) {
-
-            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
-
-            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
-
+        try (Connection conn = controller.getConnectionToDB();
+             PreparedStatement stmt = conn.prepareStatement("insert into `stores`.`employees` (id,first_name,isManager,storeID,chainID,last_name,fname) values (?,?,?,?,?,?,?);")
+        ) {
+            stmt.setString(2, employeeName);
+            stmt.setInt(1, getGenerateEmployeeID(5));
+            stmt.setBoolean(3, false);
+            stmt.setInt(4, storeID);
+            stmt.setInt(5, store.getChainIDfromStore(storeID));
+            stmt.setString(6, lastName);
+            stmt.setString(7, fname);
+            int result = stmt.executeUpdate();
+            if (result == 0) {
+                System.out.println("no updates were done");
+            } else {
+                System.out.println("number of inserted records:  " + result);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getErrorCode());
         }
 
-        return builder.toString();
+    }
+
+//    /**
+//     * add employee to chain or to store
+//     *
+//     * @param employeeName
+//     * @param toChain      - true - to chain, false = to store
+//     */
+//    public void addEmployee(String employeeName, boolean toChain) {
+//
+//        int selectedValue;
+//        try (Connection conn = controller.getConnectionToDB();
+//             PreparedStatement stmt = conn.prepareStatement("insert into `stores`.`employees` (id,first_name,isManager,storeID,chainID) values (?,?,?,?,?);");
+//        ) {
+//            stmt.setString(2, employeeName);
+//            stmt.setString(1, getGenerateEmployeeID(5));
+//
+//
+//            if (toChain) {
+//                stmt.setBoolean(3, true);
+//                stmt.setInt(4, 99);
+//
+//                chainAndMall.viewAllChains();
+//                System.out.println("Select a chain from the Available chains:  ");
+//                selectedValue = controller.selectFromScanner();
+//                stmt.setInt(5, selectedValue);
+//
+//            } else {
+//                stmt.setBoolean(3, false);
+//
+//
+//                store.viewAllStores();
+//                System.out.println("Select a Store from the Available stores:  ");
+//                selectedValue = controller.selectFromScanner();
+//                stmt.setInt(4, selectedValue);
+//
+//// set chain according to the store selected
+//
+//                stmt.setInt(5, Integer.valueOf(store.getChainIDfromStore(selectedValue)));
+//            }
+//
+//
+//            int result = stmt.executeUpdate();
+//            if (result == 0) {
+//                System.out.println("no updates were done");
+//            } else {
+//                System.out.println("number of inserted records:  " + result);
+//            }
+//
+//
+//        } catch (SQLException e) {
+//            System.out.println(e.getErrorCode());
+//        }
+//
+//    }
+
+
+    int getGenerateEmployeeID(int count) {
+
+
+        Random generator = new Random();
+        int i = generator.nextInt(50000);
+
+        return i;
     }
 
 
     public void presentAllEmployeesOfChain() {
-        int selectedValue = 0;
-            try (Connection conn = controller.getConnectionToDB();PreparedStatement stmt = conn.prepareStatement("SELECT first_name,last_name FROM stores.employees WHERE chainID=?;");
+        int selectedValue;
+        try (Connection conn = controller.getConnectionToDB(); PreparedStatement stmt = conn.prepareStatement("SELECT first_name,last_name FROM stores.employees WHERE chainID=?;");
         ) {
 
 
@@ -97,17 +155,15 @@ public class Employee {
             System.out.println("Select a chain from the Available chains:  ");
             selectedValue = controller.selectFromScanner();
             stmt.setInt(1, selectedValue);
-                try (ResultSet rs = stmt.executeQuery()) {
-                    System.out.println("Displayed Employees  :  ");
+            try (ResultSet rs = stmt.executeQuery()) {
+                System.out.println("Displayed Employees  :  ");
 
-                    while (rs.next()) {
-                        System.out.print("employee first name : " + rs.getString(1));
-                        System.out.println("  employee last name : " + rs.getString(2));
+                while (rs.next()) {
+                    System.out.print("employee first name : " + rs.getString(1));
+                    System.out.println("  employee last name : " + rs.getString(2));
 
-                    }
                 }
-
-
+            }
 
 
         } catch (SQLException e) {
