74ce1cb320af81d673b8492f3f409b97260a44f5
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-21 20:31:36 +0300

    Update menu prints

--- src/main/java/storesgroup/View.java
+++ src/main/java/storesgroup/View.java
@@ -1,31 +1,24 @@
 package storesgroup;
 
-import storesgroup.model.ChainAndMall;
-import storesgroup.model.Employee;
 import storesgroup.model.Store;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class View {
-    //
-    ChainAndMall chainAndMall = new ChainAndMall();
-    Employee employee = new Employee();
-    Controller controller = new Controller();
-    Store store;
-
-    public View() {
-
-
-    }
-
 
     public static void printMenu() {
 
         System.out.println("Please select one of the options:");
-        System.out.println("1.Create new chain");
+        System.out.println("1. Create new chain");
         System.out.println("2. Add store to chain");
         System.out.println("3. Add employee to chain");  // exist employee type name if exists and not chain update, if not exists insert
         System.out.println("4. Add employee to Store");  // exist employee type name if exists and not chain update, if not exists insert
-        System.out.println("6. Present all Employees of a certain Chain");
-        System.out.println("999. exit");
+        System.out.println("5. Present all shops of a certain shopping mall");
+        System.out.println("6. Present all shops of a certain shopping mall group");//TODO
+        System.out.println("7. Present all Employees of a certain Chain");
+        System.out.println("8. Present store details");
+        System.out.println("Any other number to exit...");
 
 
     }
@@ -33,8 +26,16 @@ public class View {
 
 
 
-    public static void printMessageToConsole(String message) {
+    public  void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printRecord(ResultSet rs, Store store) throws SQLException {
+        for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+            if (rs.getString(i) != null) {
+                printMessage(rs.getMetaData().getColumnLabel(i) + ": " + rs.getString(i));
+            }
+        }
     }
 }
 
