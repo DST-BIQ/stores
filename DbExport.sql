6280978c64551319f2ca31e098df5e8de399d600
TalBuch <37792057+TalBuch@users.noreply.github.com>
2018-04-22 11:17:06 +0300

    update sql file

--- DbExport.sql
+++ DbExport.sql
@@ -27,7 +27,7 @@ CREATE TABLE `chain` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`idchain`),
   UNIQUE KEY `idchain_UNIQUE` (`idchain`)
-) ENGINE=InnoDB AUTO_INCREMENT=10029 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10061 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `chain` (
 
 LOCK TABLES `chain` WRITE;
 /*!40000 ALTER TABLE `chain` DISABLE KEYS */;
-INSERT INTO `chain` VALUES (1,'Fox'),(10000,'Golf'),(10001,' Golf'),(10002,' Fox'),(10003,' Fox'),(10004,' Fox'),(10005,' Fox'),(10006,' Fox'),(10007,' Fox'),(10008,' Fox'),(10009,' Fox'),(10010,' Fox'),(10011,' Fox'),(10012,' Fox'),(10013,' Fox'),(10014,' Golf'),(10015,' Fox'),(10016,' Fox'),(10017,'Golf'),(10018,'Azrieli'),(10019,'Golf'),(10020,'Golf'),(10021,'Golf'),(10022,'Golf'),(10023,'Golf'),(10024,'Golf'),(10025,'aaa'),(10026,'tal'),(10027,'ddd'),(10028,'Golf');
+INSERT INTO `chain` VALUES (1,'Fox'),(10000,'Golf'),(10001,' Golf'),(10002,' Fox'),(10003,' Fox'),(10004,' Fox'),(10005,' Fox'),(10006,' Fox'),(10007,' Fox'),(10008,' Fox'),(10009,' Fox'),(10010,' Fox'),(10011,' Fox'),(10012,' Fox'),(10013,' Fox'),(10014,' Golf'),(10015,' Fox'),(10016,' Fox'),(10017,'Golf'),(10018,'Azrieli'),(10019,'Golf'),(10020,'Golf'),(10021,'Golf'),(10022,'Golf'),(10023,'Golf'),(10024,'Golf'),(10025,'aaa'),(10026,'tal'),(10027,'ddd'),(10028,'Golf'),(10029,'TTT'),(10030,'1'),(10031,'The'),(10032,'The'),(10033,'Tal'),(10034,'VVVVVV'),(10035,'McDonalds'),(10036,'Test'),(10037,'tesst'),(10038,'test1524370938352'),(10047,'Test Chain'),(10049,'Test Chain'),(10051,'Test Chain'),(10059,'test1524372412800');
 /*!40000 ALTER TABLE `chain` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +52,7 @@ CREATE TABLE `cities` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,6 +61,7 @@ CREATE TABLE `cities` (
 
 LOCK TABLES `cities` WRITE;
 /*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (1,'TLV'),(2,'Haifa');
 /*!40000 ALTER TABLE `cities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,7 +76,7 @@ CREATE TABLE `employees` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(75) NOT NULL,
-  `fname` varchar(50) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
   `dateofbirth` date DEFAULT NULL,
   `isManager` tinyint(4) DEFAULT NULL,
   `chainID` int(11) DEFAULT NULL,
@@ -86,7 +87,7 @@ CREATE TABLE `employees` (
   KEY `store_idx` (`storeID`),
   CONSTRAINT `chain` FOREIGN KEY (`chainID`) REFERENCES `chain` (`idchain`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `store` FOREIGN KEY (`storeID`) REFERENCES `stores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=39397768 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=39397783 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +96,7 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (1,'Tal','Buchshreiber','The Man',NULL,NULL,NULL,NULL),(2,'Stav','Kalo','Hertzel',NULL,NULL,NULL,NULL),(3,'t','b','ttt',NULL,NULL,NULL,NULL),(4,' Tal','Buch','Buch',NULL,NULL,10001,NULL),(123456,'Tal','BBB','B',NULL,NULL,10027,NULL),(12345678,'Tal','B','B',NULL,NULL,1,NULL),(39397700,'Tal','Buch','Buch',NULL,NULL,10001,NULL),(39397718,'Tal','Buch','Buch',NULL,NULL,10001,NULL),(39397767,'Tal','Buch','Buch',NULL,NULL,10001,NULL);
+INSERT INTO `employees` VALUES (1,'Tal','Buchshreiber','The Man',NULL,NULL,NULL,NULL),(2,'Stav','Kalo','Hertzel',NULL,NULL,NULL,NULL),(3,'t','b','ttt',NULL,NULL,NULL,NULL),(4,' Tal','Buch','Buch',NULL,NULL,10001,NULL),(123456,'Tal','BBB','B',NULL,NULL,10027,NULL),(12345678,'Tal','B','B',NULL,NULL,1,NULL),(39397700,'Tal','Buch','Buch',NULL,NULL,10001,NULL),(39397718,'Tal','Buch','Buch',NULL,NULL,10001,NULL),(39397767,'Tal','Buch','Buch',NULL,NULL,10001,NULL),(39397771,'T','B',NULL,NULL,1,10034,NULL),(39397772,'Tal','Hertzel',NULL,NULL,0,NULL,20),(39397773,'Tal','Ben','Sason',NULL,1,10037,NULL),(39397774,'Tal','M','S',NULL,0,NULL,25);
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,7 +112,7 @@ CREATE TABLE `mallgroups` (
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,6 +121,7 @@ CREATE TABLE `mallgroups` (
 
 LOCK TABLES `mallgroups` WRITE;
 /*!40000 ALTER TABLE `mallgroups` DISABLE KEYS */;
+INSERT INTO `mallgroups` VALUES (1,'Azrieli Group');
 /*!40000 ALTER TABLE `mallgroups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,7 +154,7 @@ CREATE TABLE `shoppingmalls` (
 
 LOCK TABLES `shoppingmalls` WRITE;
 /*!40000 ALTER TABLE `shoppingmalls` DISABLE KEYS */;
-INSERT INTO `shoppingmalls` VALUES (1,'Azrieli',NULL,'Hagana',NULL,NULL),(2,'Arim',NULL,NULL,NULL,NULL);
+INSERT INTO `shoppingmalls` VALUES (1,'Azrieli',NULL,'Hagana',1,NULL),(2,'Arim',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `shoppingmalls` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,6 +172,7 @@ CREATE TABLE `stores` (
   `cityId` int(11) DEFAULT NULL,
   `StreetAddress` varchar(45) DEFAULT NULL,
   `mallId` int(11) DEFAULT NULL,
+  `storeNumberInMall` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `idchain_idx` (`chain`),
@@ -178,7 +181,7 @@ CREATE TABLE `stores` (
   CONSTRAINT `chainId` FOREIGN KEY (`chain`) REFERENCES `chain` (`idchain`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `cityId` FOREIGN KEY (`cityId`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `mallId` FOREIGN KEY (`mallId`) REFERENCES `shoppingmalls` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,35 +190,8 @@ CREATE TABLE `stores` (
 
 LOCK TABLES `stores` WRITE;
 /*!40000 ALTER TABLE `stores` DISABLE KEYS */;
-INSERT INTO `stores` VALUES (3,' Fox kids',10001,NULL,NULL,1),(4,'Fox kids',10001,NULL,NULL,1),(5,'Fox kids',10001,NULL,NULL,1),(8,'Fox kids',10001,NULL,NULL,NULL),(10,'Fox kids',10001,NULL,NULL,NULL),(12,'Fox kids',10001,NULL,NULL,NULL),(14,'Fox kids',10001,NULL,NULL,NULL),(16,'Fox kids',10001,NULL,NULL,NULL),(17,'qqq',10019,NULL,NULL,NULL),(18,'Mango',10026,NULL,NULL,NULL),(20,'Fox kids',10001,NULL,NULL,NULL);
+INSERT INTO `stores` VALUES (3,' Fox kids',10001,1,NULL,1,NULL),(4,'Fox kids',10001,NULL,NULL,1,NULL),(5,'Fox kids',10001,NULL,NULL,1,NULL),(8,'Fox kids',10001,NULL,NULL,NULL,NULL),(10,'Fox kids',10001,NULL,NULL,NULL,NULL),(12,'Fox kids',10001,NULL,NULL,NULL,NULL),(14,'Fox kids',10001,NULL,NULL,NULL,NULL),(16,'Fox kids',10001,NULL,NULL,NULL,NULL),(17,'qqq',10019,NULL,NULL,1,NULL),(18,'Mango',10026,NULL,NULL,1,NULL),(20,'Fox kids',10001,NULL,NULL,NULL,NULL),(21,'Castro',10034,NULL,NULL,1,NULL),(23,'Avazi',10035,NULL,NULL,NULL,NULL),(24,'Milswanka',10036,NULL,NULL,NULL,NULL),(25,'teststore',10033,NULL,NULL,NULL,NULL),(30,'Test Store',1,NULL,NULL,NULL,NULL),(32,'Test Store',1,NULL,NULL,NULL,NULL),(34,'Test Store',1,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `stores` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `stores_in_malls`
---
-
-DROP TABLE IF EXISTS `stores_in_malls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stores_in_malls` (
-  `mallId` int(11) NOT NULL,
-  `storeId` int(11) NOT NULL,
-  `storeNumberInMall` int(11) NOT NULL,
-  PRIMARY KEY (`mallId`,`storeId`),
-  KEY `store_idx` (`storeId`),
-  CONSTRAINT `mall` FOREIGN KEY (`mallId`) REFERENCES `shoppingmalls` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `storeId` FOREIGN KEY (`storeId`) REFERENCES `stores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stores_in_malls`
---
-
-LOCK TABLES `stores_in_malls` WRITE;
-/*!40000 ALTER TABLE `stores_in_malls` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stores_in_malls` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -227,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-19 21:10:17
+-- Dump completed on 2018-04-22 11:15:15
