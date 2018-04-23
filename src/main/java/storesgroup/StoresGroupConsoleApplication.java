2f3fa803fb2c637be6a9355b9c8310f2e15f7fd8
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-04-23 11:09:07 +0300

    Create sub-methods for switch section

--- src/main/java/storesgroup/StoresGroupConsoleApplication.java
+++ src/main/java/storesgroup/StoresGroupConsoleApplication.java
@@ -36,90 +36,100 @@ public class StoresGroupConsoleApplication {
             String valueForInput;
             selection = controller.getIntFromScanner();
             switch (selection) {
-//TODO - divide to sub methods
                 case 1:
                     // create new chain
-                    view.printMessage("please enter a name for the new chain");
-                    chainAndMall.createChain(controller.getStringFromReader());
+                    createNewChain();
                     break;
                 case 2:
 // add store to chain
-                    view.printMessage("enter store name");
-                    valueForInput = controller.getStringFromReader();
-                    view.printMessage("Select chain id from list bellow");
-                    chainAndMall.viewAllChains();
-                    int storeId = controller.getIntFromScanner();
-                    store.addStoreToChain(valueForInput,storeId);
+                    addStore();
                     break;
                 case 3:
 // add employee to chain
-                    view.printMessage("enter employee first name");
-                    String firstName = controller.getStringFromReader();
-                    view.printMessage("enter employee last name");
-                    String lastName = controller.getStringFromReader();
-                    view.printMessage("enter employee middle name");
-                    String fName = controller.getStringFromReader();
-                    view.printMessage("enter chain ID from the list bellow");
-                    chainAndMall.viewAllChains();
-                    int chainId = controller.getIntFromScanner();
-                    employee.addEmployeeToChain(firstName, chainId, lastName, fName );
+                    String firstName = getStringInput("enter employee first name");
+                    String lastName = getStringInput("enter employee last name");
+                    String fName = getStringInput("enter employee middle name");
+                    employee.addEmployeeToChain(firstName, getChainIdFromUser(), lastName, fName );
                     break;
 
                 case 4:
 //Add employee to store
-                    view.printMessage("enter employee first name");
-                    firstName = controller.getStringFromReader();
-                    view.printMessage("enter employee last name");
-                    lastName = controller.getStringFromReader();
-                    view.printMessage("enter employee middle name");
-                    fName = controller.getStringFromReader();
-                    view.printMessage("enter store ID from the list bellow");
-                    store.viewAllStores();
-                    storeId = controller.getIntFromScanner();
-                    employee.addEmployeeToStore(firstName, storeId, lastName, fName);
+                    firstName = getStringInput("enter employee first name");
+                    lastName = getStringInput("enter employee last name");
+                    fName = getStringInput("enter employee middle name");
+                    employee.addEmployeeToStore(firstName, getStoreIdFromUser(), lastName, fName);
                     break;
 
                 case 5:
 // Present all shops of a mall
-                    view.printMessage("Please enter a mall id from the list bellow:");
-                    chainAndMall.viewAllMalls();
-                    int mallId = controller.getIntFromScanner();
-                    store.presentStoresInMall(mallId);
+                    store.presentStoresInMall(getMallIdFromUser());
                     break;
 
                 case 6:
 //Present all shops in mall group
-                    view.printMessage("Please enter requested mall group id from the list bellow");
-                    chainAndMall.viewAllMallGroups();
-                    int mallGroupId = controller.getIntFromScanner();
-                    store.presentStoreInMallGroup(mallGroupId);
+                    store.presentStoreInMallGroup(getMallGroupIdFromUser());
                     break;
 
                 case 7:
 //Present all Employees of a certain Chain
-                    view.printMessage("Please enter a chain id from the list bellow:");
-                    chainAndMall.viewAllChains();
-                    chainId = controller.getIntFromScanner();
-                    employee.presentAllEmployeesOfChain(chainId);
+                    employee.presentAllEmployeesOfChain(getChainIdFromUser());
                     break;
 
                 case 8:
 //Present store details
-                    view.printMessage("Please enter a store id from the list bellow:");
-                    store.viewAllStores();
-                    view.printMessage("Please enter store ID");
-                    store.presentStoreDetails(controller.getIntFromScanner());
+                    store.presentStoreDetails(getStoreIdFromUser());
                     break;
 
                 default:
                     System.out.println("Thank you and good bye..");
                     return;
-
             }
-
         }
         controller.tearDown();
 
+    }
+
+    private int getMallGroupIdFromUser() throws SQLException {
+        view.printMessage("Please enter requested mall group id from the list bellow");
+        chainAndMall.viewAllMallGroups();
+        return controller.getIntFromScanner();
+    }
+
+    private int getMallIdFromUser() throws SQLException {
+        view.printMessage("Please enter a mall id from the list bellow:");
+        chainAndMall.viewAllMalls();
+        return controller.getIntFromScanner();
+    }
+
+    private int getStoreIdFromUser() throws SQLException {
+        view.printMessage("enter store ID from the list bellow");
+        store.viewAllStores();
+        return controller.getIntFromScanner();
+    }
+
+    private int getChainIdFromUser() throws SQLException {
+        view.printMessage("enter chain ID from the list bellow");
+        chainAndMall.viewAllChains();
+        return controller.getIntFromScanner();
+    }
+
+    private String getStringInput(String s) {
+        view.printMessage(s);
+        return controller.getStringFromReader();
+
+    }
+
+    private void createNewChain() throws SQLException {
+        view.printMessage("please enter a name for the new chain");
+        chainAndMall.createChain(controller.getStringFromReader());
+    }
+
+    private void addStore() throws SQLException {
+        String storeName;
+        view.printMessage("enter store name");
+        storeName = controller.getStringFromReader();
+        int storeId = getChainIdFromUser();
+        store.addStoreToChain(storeName,storeId);
     }
 }
 
